f7e586150a47c248059089fba0e4cd9a5fb01597
AsadMalik26 <84629119+AsadMalik26@users.noreply.github.com>
2022-07-11 16:35:17 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="container">
-      <button>a button</button>
-      <Button>a bootstrap button</Button>
-      <h1>Hello from react compnent</h1>
-      <p>lorem</p>
+    <div>
+      <h1></h1>
     </div>
   );
 }
